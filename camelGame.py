54a46e0700da702eafe6e6fd0f04ff4649dd6a49
Ann Root <ann.root@bvsd.org>
2024-10-15 11:14:35 -0600

    Create camelGame.py

---
+++ camelGame.py
@@ -0,0 +1,5 @@
+
+
+print("Welcome to Camel!")
+print("You have stolen a camel to make your way across the great Mobi desert.\nThe camel's owners want their camel back and are chasing you down! Survive your\ndesert trek and outrun the owners.")
+ 
